b7e48e22510a9abefb45199d7d995ea7afb28a06
unknown <jithanroyjony@gmail.com>
2022-06-21 08:12:30 +0100

    first

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import App from './components/App';
+import reducers from './reducers'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+      <Provider store={createStore(reducers)}>
+          <App />
+      </Provider>
+  </React.StrictMode>
+);
+
